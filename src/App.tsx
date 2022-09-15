4399bc7d56a525c2255bd88a611656745c766ce6
David Micheau <david.micheau@moovone.fr>
2022-09-15 17:27:13 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const canvaSize: number = 200;
+const pixelSize: number = 100;
+
+const Container = styled.div`
+  width: ${canvaSize}px;
+  background: #bbb;
+  font-size: 0;
+`;
+
+const Pixel = styled.div.attrs((props: {color: string}) => props)`
+  width: ${pixelSize}px;
+  height: ${pixelSize}px;
+  display: inline-block;
+  background-color: #${(props) => props.color};
+`;
+
+type PixelType = {
+  color?: string;
+}
 
 function App() {
+  const pixels: PixelType[] = [];
+  for (let i = 0; i < (canvaSize * canvaSize) / pixelSize; i++) {
+    pixels.push({});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {pixels.map((pixel: PixelType, index: number) => (
+          <Pixel color={Math.floor(Math.random()*16777215).toString(16)}></Pixel>
+      ))}
+    </Container>
   );
 }
 
